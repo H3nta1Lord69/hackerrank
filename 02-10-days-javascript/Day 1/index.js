81b97dbc66f08de701314d7497e331aa652362e0
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-28 23:22:58 -0500

    chore: Second day second solution

--- 02-10-days-javascript/Day 1/index.js
+++ 02-10-days-javascript/Day 1/index.js
@@ -58,3 +58,53 @@ function getPerimeter(length, width) {
 
   return perimeter;
 }
+
+/**
+ * ========================================================
+ */
+
+("use strict");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+  var [i, vowels, cons] = [0, "aeiou", ""];
+  for (i in s) {
+    if (vowels.includes(s.charAt(i))) console.log(s.charAt(i));
+    else cons += s[i] + "\n";
+  }
+  console.log(cons.trim());
+}
+
+function main() {
+  const s = readLine();
+
+  vowelsAndConsonants(s);
+}
