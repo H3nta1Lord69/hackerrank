89e9dd42fe59c08ea3d2cf1c9f9e364664186585
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-03-08 23:21:45 -0500

    chore: Create button exercise solved

--- 02-10-days-javascript/Day 8/index.js
+++ 02-10-days-javascript/Day 8/index.js
@@ -4,6 +4,20 @@
  * ========================================================
  */
 
+var btn = document.createElement("Button");
+
+btn.innerHTML = "0";
+
+btn.id = "btn";
+
+btn.className = "btnClass";
+
+document.body.appendChild(btn);
+
+btn.onclick = function () {
+  btn.innerHTML += 2;
+};
+
 /**
  * ========================================================
  * Buttons Container
